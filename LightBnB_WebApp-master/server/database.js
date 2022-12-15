9a828adcd718a8bc5b3d88c1dd788f663d9b02c4
Kennie <Kenneth Chan>
2022-12-15 14:56:11 -0500

    refactor getUserByEmail to query data

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,19 +16,40 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+const getUserWithEmail = function (email) {
+  const userEmail = email.toLowerCase();
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1;`, [userEmail])
+    .then((result) => { return result.rows[0] })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
+
+// let user;
+// for (const userId in users) {
+//   user = users[userId];
+//   if (user.email.toLowerCase() === email.toLowerCase()) {
+//     break;
+//   } else {
+//     user = null;
+//   }
+// }
+// return Promise.resolve(user);
+
+// const getAllProperties = (options, limit = 10) => {
+//   return pool
+//     .query(`SELECT * FROM properties LIMIT $1`, [limit])
+//     .then((result) => {
+//       console.log(result.rows);
+//       return result.rows;
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// };
 
 /**
  * Get a single user from the database given their id.
